d97b346062a1d7685d8861355e8ec86c120f3fa1
Rostyslav Pidburachynskyi <gamedevrostik@gmail.com>
2020-10-17 19:11:17 +0300

    fixed commit

--- shared/dist/socketHandlers/setTimeHandler.d.ts
+++
@@ -1,6 +0,0 @@
-import { types } from '..';
-export declare const setTimeHandler: (info: types.Info, lightId: string, time: number) => {
-    lights: import("../interfaces").ILight[];
-    isLocalControlling: boolean;
-    isTurned: boolean;
-};
