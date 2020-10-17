295379f1d883cefc99c5159226093a6e395af9dd
Rostyslav Pidburachynskyi <gamedevrostik@gmail.com>
2020-10-17 18:49:00 +0300

    added setTimeHandler

--- shared/dist/index.js
+++ shared/dist/index.js
@@ -1,5 +1,6 @@
 "use strict";
 exports.__esModule = true;
-exports.interfaces = exports.types = void 0;
+exports.handlers = exports.interfaces = exports.types = void 0;
 exports.types = require("./types");
 exports.interfaces = require("./interfaces");
+exports.handlers = require("./handlers");
