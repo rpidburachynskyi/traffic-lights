922633ffdc3b1494ee6f75e42aa1f76858fbb9bd
Rostyslav Pidburachynskyi <gamedevrostik@gmail.com>
2020-10-17 19:19:32 +0300

    added clamp and unclamp actions

--- server/src/socket.io/connectActions.ts
+++ server/src/socket.io/connectActions.ts
@@ -5,6 +5,8 @@ import { updateAll } from './socket.io';
 export const connectActions = (socket: SocketIO.Socket) => {
 	socket.on('turn', onTurn);
 	socket.on('setTime', onSetTime);
+	socket.on('clamp', onClamp);
+	socket.on('unclamp', onUnlamp);
 };
 
 const onTurn = (isTurned: boolean) => {
@@ -17,5 +19,15 @@ const onTurn = (isTurned: boolean) => {
 
 const onSetTime = (lightId: string, time: number) => {
 	updateInfo((info) => handlers.setTimeHandler(info, lightId, time));
+	updateAll();
+};
+
+const onClamp = (lightId: string) => {
+	updateInfo((info) => handlers.clampHandler(info, lightId));
+	updateAll();
+};
+
+const onUnlamp = (lightId: string) => {
+	updateInfo((info) => handlers.unclampHandler(info, lightId));
 	updateAll();
 };
