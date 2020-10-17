6b07fb63c24d96705ce388cf299cba9fa4d64703
Rostyslav Pidburachynskyi <gamedevrostik@gmail.com>
2020-10-17 18:58:18 +0300

    use setTimeHandler

--- server/src/socket.io/connectActions.ts
+++ server/src/socket.io/connectActions.ts
@@ -1,8 +1,10 @@
+import { handlers } from 'shared';
 import { updateInfo } from '../info';
 import { updateAll } from './socket.io';
 
 export const connectActions = (socket: SocketIO.Socket) => {
 	socket.on('turn', onTurn);
+	socket.on('setTime', onSetTime);
 };
 
 const onTurn = (isTurned: boolean) => {
@@ -10,5 +12,10 @@ const onTurn = (isTurned: boolean) => {
 		...info,
 		isTurned
 	}));
+	updateAll();
+};
+
+const onSetTime = (lightId: string, time: number) => {
+	updateInfo((info) => handlers.setTimeHandler(info, lightId, time));
 	updateAll();
 };
