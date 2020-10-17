00253da63289a768a114fd84fbda748863252318
Rostyslav Pidburachynskyi <gamedevrostik@gmail.com>
2020-10-17 16:20:33 +0300

    setup initial main.cpp

---
+++ controller/src/main.cpp
@@ -0,0 +1,31 @@
+#include <Arduino.h>
+
+#include "counter/counter.h"
+#include "traffic-lights/traffic-lights.h"
+
+const int CLK = 12;
+const int DIO = 14;
+
+const int RED_PIN = 16;
+const int YELLOW_PIN = 32;
+const int GREEN_PIN = 33;
+const int LEFT_GREEN_PIN = 25;
+const int RIGHT_GREEN_PIN = 26;
+
+Counter counter(CLK, DIO);
+TrafficLights trafficLights(RED_PIN, YELLOW_PIN, GREEN_PIN, LEFT_GREEN_PIN, RIGHT_GREEN_PIN);
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("RostiKTawer");
+}
+
+void loop()
+{
+  delay(1000);
+
+  trafficLights.turnRed(true);
+  delay(1000);
+  trafficLights.turnRed(false);
+}
