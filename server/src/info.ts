81dbc95ade9c596eb5bd97cf0ab3e080a97b1879
Rostyslav Pidburachynskyi <gamedevrostik@gmail.com>
2020-10-17 18:58:05 +0300

    added light items

--- server/src/info.ts
+++ server/src/info.ts
@@ -1,9 +1,40 @@
 import { types } from 'shared';
 import { ILight } from 'shared/dist/interfaces';
 
-let lights: ILight[] = [];
+let lights: ILight[] = [
+	{
+		id: 'red',
+		clamped: false,
+		color: 'red',
+		duration: 10
+	},
+	{
+		id: 'yellow',
+		clamped: false,
+		color: 'yellow',
+		duration: 10
+	},
+	{
+		id: 'green',
+		clamped: false,
+		color: 'green',
+		duration: 10
+	},
+	{
+		id: 'leftGreen',
+		clamped: false,
+		color: 'green',
+		duration: 10
+	},
+	{
+		id: 'rightGreen',
+		clamped: false,
+		color: 'green',
+		duration: 10
+	}
+];
 
-const info: types.Info = {
+let info: types.Info = {
 	isTurned: true,
 	isLocalControlling: false,
 	lights
@@ -11,4 +42,12 @@ const info: types.Info = {
 
 export const getInfo = () => {
 	return info;
+};
+
+export const updateInfo = (callback: (oldInfo: types.Info) => types.Info) => {
+	const newInfo = callback(info);
+	info = {
+		...info,
+		...newInfo
+	};
 };
