82f82adeebbd831ac9e4814de8b7ee8b57c77f80
Rostyslav Pidburachynskyi <gamedevrostik@gmail.com>
2020-10-17 18:58:44 +0300

    modified light item view & added moment.js

--- web/src/components/LightListItem/LightListItem.tsx
+++ web/src/components/LightListItem/LightListItem.tsx
@@ -1,17 +1,34 @@
 import { Button, Form, InputNumber, List, TimePicker, Typography } from 'antd';
 import React from 'react';
-import { ILight } from '../../interfaces/ILight';
+import { interfaces } from 'shared';
 import classes from './LightListItem.module.scss';
+import moment from 'moment';
+import { setTime } from '../../services/socket/actions';
 
 interface Props {
-	light: ILight;
+	light: interfaces.ILight;
 }
 
 const LightListItem = ({ light }: Props) => {
+	const onChangeTime = (momentDate: moment.Moment | null) => {
+		if (!momentDate) throw new Error('Unknown date');
+		const time = momentDate.minutes() * 60 + momentDate.seconds();
+		setTime(light.id, time);
+	};
+
 	return (
 		<List.Item className={classes.item}>
 			<Typography.Title level={5}>Light</Typography.Title>
-			<TimePicker showHour={false} showNow={false} />
+			<TimePicker
+				showHour={false}
+				showNow={false}
+				format='mm:ss'
+				value={moment(
+					`${Math.floor(light.duration / 60)}:${light.duration % 60}`,
+					'mm:ss'
+				)}
+				onChange={onChangeTime}
+			/>
 			<Button loading={true}>
 				{light.clamped ? 'Clamped' : 'Clamp'}
 			</Button>
