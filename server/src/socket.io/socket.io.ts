a85fe9d01d580e3a04949aa9ea86fd021969d4e8
Rostyslav Pidburachynskyi <gamedevrostik@gmail.com>
2020-10-17 18:24:36 +0300

    connected actions to sockets

--- server/src/socket.io/socket.io.ts
+++ server/src/socket.io/socket.io.ts
@@ -1,6 +1,9 @@
 import { Server } from 'http';
 import * as socketIo from 'socket.io';
 import { getInfo } from '../info';
+import { connectActions } from './connectActions';
+
+let sockets: SocketIO.Socket[] = [];
 
 export const connect = (httpServer: Server) => {
 	const io = socketIo(httpServer);
@@ -11,7 +14,19 @@ export const connect = (httpServer: Server) => {
 const onConnect = (socket: socketIo.Socket) => {
 	const info = getInfo();
 
+	sockets.push(socket);
+
 	socket.emit('init', {
 		info
+	});
+
+	connectActions(socket);
+};
+
+export const updateAll = () => {
+	sockets.forEach((socket) => {
+		const info = getInfo();
+
+		socket.emit('update', { info });
 	});
 };
