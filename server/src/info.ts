a9aae14e6660f7e859e8d8eebd89ad9daa1f8c89
Rostyslav Pidburachynskyi <gamedevrostik@gmail.com>
2020-10-18 10:25:33 +0300

    added links to lights

--- server/src/info.ts
+++ server/src/info.ts
@@ -4,33 +4,48 @@ import { ILight } from 'shared/dist/interfaces';
 let lights: ILight[] = [
 	{
 		id: 'red',
+		name: 'Red',
 		clamped: false,
 		color: 'red',
-		duration: 10
+		duration: 10,
+		spec: false,
+		linksWithId: ''
 	},
 	{
 		id: 'yellow',
+		name: 'Yellow',
 		clamped: false,
 		color: 'yellow',
-		duration: 10
+		duration: 10,
+		spec: false,
+		linksWithId: ''
 	},
 	{
 		id: 'green',
+		name: 'Green',
 		clamped: false,
 		color: 'green',
-		duration: 10
+		duration: 10,
+		spec: false,
+		linksWithId: ''
 	},
 	{
 		id: 'leftGreen',
+		name: 'Left green',
 		clamped: false,
 		color: 'green',
-		duration: 10
+		duration: 10,
+		spec: true,
+		linksWithId: 'green'
 	},
 	{
 		id: 'rightGreen',
+		name: 'Right green',
 		clamped: false,
 		color: 'green',
-		duration: 10
+		duration: 10,
+		spec: true,
+		linksWithId: 'green'
 	}
 ];
 
--- server/src/socket.io/connectActions.ts
+++ server/src/socket.io/connectActions.ts
@@ -1,12 +1,13 @@
 import { handlers } from 'shared';
 import { updateInfo } from '../info';
-import { updateAll } from './socket.io';
+import { updateAll } from '../utils';
 
 export const connectActions = (socket: SocketIO.Socket) => {
 	socket.on('turn', onTurn);
 	socket.on('setTime', onSetTime);
 	socket.on('clamp', onClamp);
 	socket.on('unclamp', onUnlamp);
+	socket.on('changeLinkWithId', onChangeLinkWithId);
 };
 
 const onTurn = (isTurned: boolean) => {
@@ -29,5 +30,12 @@ const onClamp = (lightId: string) => {
 
 const onUnlamp = (lightId: string) => {
 	updateInfo((info) => handlers.unclampHandler(info, lightId));
+	updateAll();
+};
+
+const onChangeLinkWithId = (lightId: string, linkWithId: string) => {
+	updateInfo((info) =>
+		handlers.changeSpecLightLink(info, lightId, linkWithId)
+	);
 	updateAll();
 };
